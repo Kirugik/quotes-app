0e9745eb993871156ab840ce32be16358f0368f6
Robert Kirui <kirugik79@gmail.com>
2022-04-09 11:03:46 +0300

    Create quotes component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuotesComponent } from './quotes/quotes/quotes.component';
+import { CreateQuoteComponent } from './create-quote/create-quote/create-quote.component';
+import { FormsModule } from '@angular/forms';
+
+//others
+// import { HttpClientModule } from '@angular/common/http';
+// import { NgProgressModule } from '@ngx-progressbar/core';
+// import { NgProgressHttpClientModule } from '@ngx-progressbar/http-client'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesComponent,
+    CreateQuoteComponent
   ],
   imports: [
+    // NgProgressModule.forRoot(),
+    // NgProgressHttpClientModule,
+    // HttpClientModule,
+
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    FontAwesomeModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
