2a053f0075b25e85f77dbab6a513d5543e28dd0d
Robert Kirui <kirugik79@gmail.com>
2022-04-09 23:16:16 +0300

    Add global css styles

--- src/app/quotes/quotes/quotes.component.ts
+++ src/app/quotes/quotes/quotes.component.ts
@@ -13,6 +13,7 @@ export class QuotesComponent implements OnInit {
   downvotes = 0;
   isComplete= true;
 
+  // quotes array
   quotes: Quotes[]=[
     new Quotes(0,'Socrates','“The unexamined life is not worth living”',0,0),
     new Quotes(1,'Mark Twain','“The fear of death follows from the fear of life. A man who lives fully is prepared to die at any time.”',0,0),
@@ -20,23 +21,31 @@ export class QuotesComponent implements OnInit {
     new Quotes(3,'Steve Jobs','“The ones who are crazy enough to think that they can change the world, are the ones who do.”',0,0), 
   ]
 
+  // display the quotes on the page
   getCurrentId(){
     return this.quotes.length +1
   }
   
+  // create new quote using the form
   newQuote(quote:any) {
     quote.id = this.getCurrentId();
     quote.author= quote.author;
     quote.quote=quote.quote;
     this.quotes.push(quote);
   }
+
+  // upvotes counter 
   upVote(quote:any){
     quote.upvote= quote.upvote+1;
     console.log('quote.upvote')
   }
+
+  // downvotes counter 
   downVote(quote:any){
     quote.downvote = quote.downvote+1;
   }
+
+  // delete quote
   quoteDelete(isComplete:any, index:any){
     if (isComplete) {
       let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].quote}?`)
