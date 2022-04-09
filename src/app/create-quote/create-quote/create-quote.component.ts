2e7cc6386e393a14a69b70c161bd5a78bb1c189d
Robert Kirui <kirugik79@gmail.com>
2022-04-09 11:20:01 +0300

    Create form for adding new quotes

--- src/app/create-quote/create-quote/create-quote.component.ts
+++ src/app/create-quote/create-quote/create-quote.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quotes } from '../../quotes'; 
 
 @Component({
   selector: 'app-create-quote',
   templateUrl: './create-quote.component.html',
   styleUrls: ['./create-quote.component.css']
 })
+
 export class CreateQuoteComponent implements OnInit {
+  newQuote = new Quotes(0,'','',0,0);
+
+  @Output() addQuote= new EventEmitter<Quotes>();
+
+    submitQuote(){
+      console.log(this.newQuote)
+      this.addQuote.emit(this.newQuote);
+      
+    }
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-}
+} 
